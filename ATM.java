73b2ca742cd3a3cd12293b68ab3c8d236d3fee60
Eeham Khan <eehamkhan@hotmail.com>
2023-01-14 14:39:51 -0500

    Scanner bug "fixed" by not closing the other instances of Scanner. Code cleanup and bug fixes.

--- ATM.java
+++ ATM.java
@@ -20,14 +20,13 @@ class bankInfo {
             System.out.println("Please enter your name: ");
             name = in.next();
 
-            in.close();
-
             System.out.println("Your starting balance has been set at 0$");
             balance = 0;
 
     }
 
     public void displayAccount() {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Please enter PIN to verify identity: ");
@@ -40,62 +39,82 @@ class bankInfo {
         } else {
             System.out.println("Wrong PIN. Account details unavailable.");
         }
-
-        in.close();
     }
 
     public void deposit() {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter the amount you would like to deposit into your account: ");
         long amount = in.nextLong();
-        in.close();
 
         balance += amount;
+        System.out.println("You have successfully deposited " + amount + "$ into your account.\nYour current balance is " + balance + "$\n");
+        
     }
 
     public void withdrawal() {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter the amount you would like to withdraw from your account: ");
         long amount = in.nextLong();
-        in.close();
 
-        balance += amount;
+        balance -= amount;
+
+        System.out.println("You have successfully withdrawn " + amount + "$ into your account.\nYour current balance is " + balance + "$\n");
+
     }
-
+    
+    
 }
 
 public class ATM {
 
     public static void main(String[] args) {
+
+        boolean value = false;
+
         Scanner input = new Scanner(System.in);
+        int choice;
 
         bankInfo customer = new bankInfo();
         customer.openAccount();
 
         System.out.println("\n ***ATM System Starting***");
-        System.out.println("1. Display all account details \n 2. Deposit money \n 3. Withdraw money \n 4.Exit ");
-        System.out.println("Please enter your selection: ");
-        int choice = input.nextInt();
+        System.out.println("1. Display all account details \n2. Deposit money \n3. Withdraw money \n4.Exit ");
+        System.out.println("\nPlease enter your selection: ");
+        choice = input.nextInt();
 
         do {
+
+            if (value == true) {
+            System.out.println("1. Display all account details \n2. Deposit money \n3. Withdraw money \n4. Exit ");
+            System.out.println("\nPlease enter your selection: ");
+            choice = input.nextInt();
+            }
+
             switch (choice) {
                 case 1:
                     customer.displayAccount();
+                    value = true;
                     break;
                 case 2:
                     customer.deposit();
+                    value = true;
                     break;
                 case 3:
                     customer.withdrawal();
+                    value = true;
                     break;
                 case 4:
                     System.out.println("Thank you for using the ATM system. See you soon.");
+                    value = false;
                     break;
             }
 
         } while (choice != 4);
+
         input.close();
     }
 } 
